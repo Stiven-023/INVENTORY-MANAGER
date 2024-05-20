003fc582aef98f072797e55246d527c0aed5ae71
OviedoSebastian <114544540+OviedoSebastian@users.noreply.github.com>
2024-05-20 10:09:20 -0500

    Update settings.py

--- backend/App/settings.py
+++ backend/App/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
                      
 # Application definition
